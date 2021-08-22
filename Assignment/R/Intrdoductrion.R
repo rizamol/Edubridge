ec201e0224f45443694e8d6bcd419536aad9aa62
Riza mol <85162875+rizamol@users.noreply.github.com>
2021-08-22 20:56:22 +0530

    Add files via upload

---
+++ Assignment/R/Intrdoductrion.R
@@ -0,0 +1,47 @@
+v<-c('riza','malu','jimmy')
+print(v)
+print(class(v))
+s<-1:9
+print(s)
+print(class(s))
+print(typeof(s))
+print(length(v))
+print(v[2:3])
+print(v[1])
+print(sort(v))
+v[1]="keerthi"
+print(v)
+r<-rep(c(5,7,4),times = 3)
+print(r)
+r<-rep(c(3,2,7),times=c(4,2,3))
+print(r)
+print(v[c(-3)])
+
+
+
+a<-c('john','veena','thasni','anza')
+print(a)
+length((a))
+print(class(a))
+print(typeof(a))
+print(a[2:4])
+print(sort(a))
+print(sort(a),decreasing=TRUE)
+
+a1<-rep(c(5,6,2),times = 2)
+
+a1<-rep(c(5,6,2),times = 3)
+
+a1<-rep(c(0,7,7),times =c(3,1,2))
+
+
+
+
+
+
+
+
+
+
+
+
